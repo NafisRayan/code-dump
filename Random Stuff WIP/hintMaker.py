4e9b487ab489a4defd49eedc0e45a6038814b5c6
Nafis Rayan <90589446+NafisRayan@users.noreply.github.com>
2024-04-17 12:46:28 +0600

    Update hintMaker.py

--- Random Stuff WIP/hintMaker.py
+++ Random Stuff WIP/hintMaker.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 import json
 import time
 
-genai.configure(api_key="API_KEY")
+genai.configure(api_key="API")
 
 # Set up the model
 generation_config = {
@@ -37,37 +37,48 @@ model = genai.GenerativeModel(model_name="gemini-pro",
 
 
 # Read the JSON file
-with open('output.json', 'r') as file:
+with open('./helper/updatedHints.json', 'r') as file:
     data = json.load(file)
 
-
 c=0
+wordCount = 0
 # Iterate over all keys at the top level of the JSON structure
 for key in data:
     # Iterate over each item in the list associated with the current key
     for item in data[key]:
+        
+        # if wordCount>=2341:
         word = item['word']
-        item['hint'] = ''
-        hint = item['hint']
 
         user_input = f'''You have to generate a hint for the word {word} make sure to make the hint for a {key} level. 
         Do not mention the word in the hint. Also make the hint ambiguous to make it hard for the user to understand.
         And your reply should only be the short hint nothing more.'''
 
         if item['hint']=='':
-            response = model.generate_content(user_input).text
-            print("Bot:", response)
-            item['hint'] = response
+            try:
+                response = model.generate_content(user_input).text
+                # Assuming 'response' is a string or has a 'text' attribute that can be checked for emptiness
+                if response and response.strip(): # Check if response is not empty and not just whitespace
+                    print("Bot:", response)
+                    item['hint'] = response
+                else:
+                    print("No response generated for word:", word)
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                wordCount+=1
+                
+                # Handle the error, e.g., by logging it, retrying, or setting a default response
 
             # Write the modified JSON back to a file
-            with open('output.json', 'w') as file:
+            with open('./helper/output.json', 'w') as file:
                 json.dump(data, file, indent=4)
 
             time.sleep(1)
             
-        c+=1
-        print('loading = ',c)
+            c+=1
+        print('count = ',c)
+        print('loading = ',wordCount)
         print(item)
         
 
-print("Modified JSON has been written to 'output.json'.")
+print("Modified JSON has been written")
